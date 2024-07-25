3bfe744dcdf47c92fb8a9a14e2283ec6778ce4d7
Mac21 <russ.tolle@gmail.com>
2024-07-25 14:21:25 -0600

    Add skeleton project structure

---
+++ skeleton/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+)
+
+func displayTestResults[T cmp.Ordered](answer, result T) {
+	fmt.Printf("Test success: %v? Expected %v got %v\n\n", cmp.Compare(answer, result) == 0, answer, result)
+}
+
+func displaySliceTestResults[T ~[]E, E cmp.Ordered](answer, result T) {
+	fmt.Printf("Test success: %v? Expected %v got %v\n\n", slices.Compare(answer, result) == 0, answer, result)
+}
+
+func main() {
+	result := 0
+	answer := 1
+	displayTestResults(answer, result)
+}
