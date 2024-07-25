f3207cb69e22b6acf0a0235e9f56fb65ef67aa93
Mac21 <russ.tolle@gmail.com>
2024-07-25 14:47:46 -0600

    Fix git ignore

--- skeleton/main.go
+++ skeleton/main.go
@@ -7,7 +7,11 @@ import (
     "github.com/mac21/gods"
 )
 
-func displayTestResults[T cmp.Ordered](answer, result T) {
+func displayTestResults[T comparable](answer, result T) {
+	fmt.Printf("Test success: %v? Expected %v got %v\n\n", answer == result, answer, result)
+}
+
+func displayOrderedTestResults[T cmp.Ordered](answer, result T) {
 	fmt.Printf("Test success: %v? Expected %v got %v\n\n", cmp.Compare(answer, result) == 0, answer, result)
 }
 
@@ -15,6 +19,11 @@ func displaySliceTestResults[T ~[]E, E cmp.Ordered](answer, result T) {
 	fmt.Printf("Test success: %v? Expected %v got %v\n\n", slices.Compare(answer, result) == 0, answer, result)
 }
 
+/*
+Problem description:
+
+Constraints: 
+*/
 func main() {
 	result := 0
 	answer := 1
