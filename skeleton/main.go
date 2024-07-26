76a2dac46c21f7d0516b9fd6ea9a71187bc052e9
Mac21 <russ.tolle@gmail.com>
2024-07-25 21:22:29 -0600

    Change skeleton to have more boilerplate

--- skeleton/main.go
+++ skeleton/main.go
@@ -1,32 +1,9 @@
 package main
 
-import (
-	"cmp"
-	"fmt"
-	"slices"
-    "github.com/mac21/gods"
-)
-
-func displayTestResults[T comparable](input any, answer, result T) {
-    fmt.Printf("Test: %v success: %v? Expected %v got %v\n\n", input, answer == result, answer, result)
-}
-
-func displayOrderedTestResults[T cmp.Ordered](input any, answer, result T) {
-	fmt.Printf("Test %v success: %v? Expected %v got %v\n\n", input, cmp.Compare(answer, result) == 0, answer, result)
-}
-
-func displaySliceTestResults[T ~[]E, E cmp.Ordered](input any, answer, result T) {
-	fmt.Printf("Test %v success: %v? Expected %v got %v\n\n", input, slices.Compare(answer, result) == 0, answer, result)
-}
-
 /*
 Problem description:
-
 Constraints: 
 */
-func main() {
-    input := 1
-	result := 0
-	answer := 1
-	displayTestResults(input, answer, result)
+func problem(input []int) []int {
+    return input
 }
