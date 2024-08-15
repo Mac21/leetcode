c9d8d2910b39160bbc3604bce775d880bd9ea3af
Mac21 <russ.tolle@gmail.com>
2024-08-15 14:17:54 -0600

    Finish implementing algorithm however it does not generate the correct output because leetcode has a requirement about order.

--- generateparentheses/main.go
+++ generateparentheses/main.go
@@ -20,6 +20,32 @@ Constraints:
 
 1 <= n <= 8
 */
+var dp func(i int) []string
+
 func generateParenthesis(n int) []string {
-	return nil
+	cache := make([]string, 2)
+	cache[0] = ""
+	cache[1] = "()"
+	dp = func(i int) []string {
+		if i > 0 && i < len(cache) {
+			return cache[i : i+1]
+		}
+
+		pp := dp(i - 1)
+		currentLevelCacheIndex := len(cache)
+		for _, p := range pp {
+			nest := "(" + p + ")"
+			cache = append(cache, nest)
+			concatf := "()" + p
+			if nest != concatf {
+				cache = append(cache, concatf)
+			}
+			concatb := p + "()"
+			if concatf != concatb {
+				cache = append(cache, concatb)
+			}
+		}
+		return cache[currentLevelCacheIndex:]
+	}
+	return dp(n)
 }
