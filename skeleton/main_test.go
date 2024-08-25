9a9f256105ff5b563dc58e6438ba67af9f630043
Mac21 <russ.tolle@gmail.com>
2024-08-25 03:49:03 -0600

    Fix skeleton main_test.go

--- skeleton/main_test.go
+++ skeleton/main_test.go
@@ -6,27 +6,27 @@ import (
     "slices"
 )
 
-type Input[T ~[]comparable | ~comparable] struct {
-    Initial  T
-    Expected T
-    Answer T
+type Input struct {
+    Initial  any
+    Expected any
+    Answer any
 }
 
 func validateTestResults[T comparable](t *testing.T, input any, answer, result T) {
     if answer != result {
-        t.Fatalf("Test: %v success: %v? Expected %v got %v\n", input, answer == result, answer, result)
+        t.Fatalf("Test: %v Expected %v got %v\n", input, answer, result)
     }
 }
 
 func validateOrderedTestResults[T cmp.Ordered](t *testing.T, input any, answer, result T) {
     if cmp.Compare(answer, result) != 0 {
-        t.Fatalf("Test %v success: %v? Expected %v got %v\n", input, cmp.Compare(answer, result) == 0, answer, result)
+        t.Fatalf("Test %v Expected %v got %v\n", input, answer, result)
     }
 }
 
 func validateSliceTestResults[T ~[]E, E cmp.Ordered](t *testing.T, input any, answer, result T) {
     if slices.Compare(answer, result) != 0 {
-        t.Fatalf("Test %v success: %v? Expected %v got %v\n", input, slices.Compare(answer, result) == 0, answer, result)
+        t.Fatalf("Test %v Expected %v got %v\n", input, answer, result)
     }
 }
 
